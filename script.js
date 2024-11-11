11d91632d20b02d3c5d653ba6cf33dc7f6d321ac
Vincent Edwards <mister.man489@gmail.com>
2024-11-11 08:07:53 -0800

    Create function to get a choice from the user

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function getComputerChoice() {
   const choiceIdx = Math.floor(Math.random() * choices.length);
   return choices[choiceIdx];
 }
+
+function getHumanChoice() {
+  let message = "Enter the number for one of the choices below:";
+  for (let i = 0; i < choices.length; ++i) {
+    const choice = choices[i];
+    message += `\n- (${i + 1}) ${choice}`;
+  }
+
+  while (true) {
+    let choiceIdx = +prompt(message) - 1;
+
+    if (isNaN(choiceIdx) || choiceIdx < 0 || choiceIdx >= choices.length) {
+      const invalidInputMessage = "Invalid input: Please try again";
+      console.log(invalidInputMessage);
+      continue;
+    }
+
+    return choices[choiceIdx];
+  }
+}
